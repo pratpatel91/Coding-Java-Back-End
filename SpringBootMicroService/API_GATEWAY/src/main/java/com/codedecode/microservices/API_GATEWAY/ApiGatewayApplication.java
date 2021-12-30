69e8948377f1394b2ebc336d85fec3125ce53484
pratpatel91 <76264457+pratpatel91@users.noreply.github.com>
2021-12-30 15:48:34 -0600

    Add files via upload

---
+++ SpringBootMicroService/API_GATEWAY/src/main/java/com/codedecode/microservices/API_GATEWAY/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.codedecode.microservices.API_GATEWAY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
